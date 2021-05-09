d5d07040cd7f3d3d75d81f24644af7beafc98245
Partharaj Deb <partharajbd@gmail.com>
2021-05-10 02:09:51 +0600

    JS Validation in prescription creation form

--- src/main/resources/static/js/validation.js
+++ src/main/resources/static/js/validation.js
@@ -1,1 +1,29 @@
-var form = document.getElementById("create-prescription");
+var prescriptionCreateForm = document.getElementById("create-prescription");
+if (prescriptionCreateForm) {
+    prescriptionCreateForm.addEventListener("submit", function (e) {
+
+        let containsError = false;
+
+
+        let prescriptionDateInput = prescriptionCreateForm.querySelector("input[name=prescriptionDate]");
+        let prescriptionDate = prescriptionDateInput.value;
+        let today = new Date();
+        today.setHours(23,59,59);
+
+        if (prescriptionDate == "") {
+            alert("Prescription date field is required");
+            containsError = true;
+        }else if(new Date(prescriptionDateInput.value) > today){
+            alert("Prescription date should be today's or older date");
+            containsError = true;
+        }
+
+
+        // Validation for other fields goes here
+
+
+        if (containsError) {
+            e.preventDefault();
+        }
+    });
+}
